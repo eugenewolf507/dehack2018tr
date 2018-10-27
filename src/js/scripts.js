5b2e501ca98668c9f9a94a2b9c2fa1c744e15404
EugeneVolkov <eugenewolf507@gmail.com>
2018-10-28 00:14:20 +0300

    change menu, first part

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,10 +1,10 @@
-// const menuButton = document.querySelector('.js-burger');
-// const menu = document.querySelector('.burger-list');
+const menuButton = document.querySelector('.js-burger');
+const menu = document.querySelector('.burger-list');
 
-// function menuButtonClickHandler() {
-//     menu.classList.toggle('hidden')
-// };
-// menuButton.addEventListener('click', menuButtonClickHandler);
+function menuButtonClickHandler() {
+    menu.classList.toggle('hidden')
+};
+menuButton.addEventListener('click', menuButtonClickHandler);
 
 
 // -------------------
