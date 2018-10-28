cd15c20cd072a6883830a076e79b3c9005725011
EugeneVolkov <eugenewolf507@gmail.com>
2018-10-28 12:12:34 +0200

    change query selector for menu button

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,10 +1,11 @@
-const menuButton = document.querySelector('.js-burger');
+// const menuButton = document.querySelector('.js-burger');
+const menuButton = document.querySelector('.box-shadow-menu--closed');
 const menu = document.querySelector('.burger-list');
 
-// function menuButtonClickHandler() {
-//     menu.classList.toggle('hidden')
-// };
-// menuButton.addEventListener('click', menuButtonClickHandler);
+function menuButtonClickHandler() {
+    menu.classList.toggle('hidden')
+};
+menuButton.addEventListener('click', menuButtonClickHandler);
 
 
 // -------------------
