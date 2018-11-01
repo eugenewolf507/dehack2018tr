3c18db46dcc09132ebce7969ef73ec8974f5575b
Ivan <nerty777@gmail.com>
2018-11-01 18:20:26 +0200

    Remove the menu when clicking on the map

--- src/js/menu-section.js
+++ src/js/menu-section.js
@@ -1,3 +1,4 @@
+//---------------------The appearance and disappearance of the menu when you click menu button START--------------------
 const menuButton = document.querySelector(".box-shadow-menu--closed");
 const menu = document.querySelector(".burger-list");
 
@@ -13,3 +14,20 @@ function menuButtonClickHandler() {
   }
 }
 menuButton.addEventListener("click", menuButtonClickHandler);
+//---------------------The appearance and disappearance of the menu when you click menu button END--------------------
+
+//---------------------Remove the menu when clicking on the map START--------------------
+function mapClickHandlerRemoveMenu() {
+  menu.classList.remove("hidden");
+  if (!menuHandle) {
+    menuHandle = true;
+  } else {
+    menuButton.style.backgroundImage = "url('../img/menu.svg')";
+    menuHandle = false;
+  }
+}
+const map = document.getElementById("map");
+map.addEventListener("click", mapClickHandlerRemoveMenu);
+//---------------------Remove the menu when clicking on the map END--------------------
+
+
