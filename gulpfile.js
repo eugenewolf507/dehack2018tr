f1c566420ac648121f440b5d33dab9f1a7b15725
Ivan <nerty777@gmail.com>
2019-01-05 07:48:35 +0200

    browserconfig.xml

--- gulpfile.js
+++ gulpfile.js
@@ -87,7 +87,7 @@ gulp.task("svg-sprite", () => {
 gulp.task("images", () => {
   return gulp
     .src([
-      "./src/img/**/*.{png,jpg,jpeg,svg,ico,gif,json,webmanifest}",
+      "./src/img/**/*.{png,jpg,jpeg,svg,ico,gif,json,webmanifest,xml}",
       "!./src/img/sprite/**/*.*"
     ])
     .pipe(
